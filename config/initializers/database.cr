846e28bf41031bbc4d6ff267b28874c3c5a6d5c1
Anton Maminov <anton.linux@gmail.com>
2021-01-22 20:24:31 +0200

    change logger

--- config/initializers/database.cr
+++ config/initializers/database.cr
@@ -3,12 +3,9 @@ require "../../src/models/*"
 
 Clear::SQL.init(ENV["DATABASE_URL"])
 
-log_file =
-  case ENV["CRYSTAL_ENV"]
-  when "production"
-    File.new("#{__DIR__}/../../log/clear.log", "a+")
-  else
-    STDOUT
-  end
+log_file = File.new("#{__DIR__}/../../log/clear.log", "a+")
+stdout = STDOUT
 
-Log.builder.bind "clear.*", :debug, Log::IOBackend.new(log_file)
+writer = IO::MultiWriter.new(log_file, stdout)
+
+Log.builder.bind "clear.*", :debug, Log::IOBackend.new(writer)
