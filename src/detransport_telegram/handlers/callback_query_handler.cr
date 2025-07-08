af099748eb652ce1f1fa20ac8c511ecab5661f20
Anton Maminov <anton.linux@gmail.com>
2025-07-08 11:51:23 +0300

    update schedule

--- src/detransport_telegram/handlers/callback_query_handler.cr
+++ src/detransport_telegram/handlers/callback_query_handler.cr
@@ -12,15 +12,53 @@ module DetransportTelegram
 
     def handle
       if chat_id = @chat_id
-        bot.answer_callback_query(@callback_query.id, cache_time: 1)
+        callback_data = @callback_query.data
 
-        stop_id = @callback_query.data
+        return unless callback_data
 
-        keyboard = TelegramBot::ReplyKeyboardRemove.new
-        bot.send_message(chat_id, routes_text(stop_id), parse_mode: "Markdown", reply_markup: keyboard)
+        if callback_data.starts_with?("update_")
+          stop_id = callback_data.sub("update_", "").to_i
+          handle_update_routes(chat_id, stop_id)
+        else
+          stop_id = callback_data.to_i
+          handle_stop_selection(chat_id, stop_id)
+        end
       end
     end
 
+    private def handle_stop_selection(chat_id : Int64, stop_id : Int32)
+      bot.send_message(
+        chat_id: chat_id,
+        text: routes_text(stop_id),
+        parse_mode: "Markdown",
+        reply_markup: update_keyboard(stop_id)
+      )
+    end
+
+    private def handle_update_routes(chat_id : Int64, stop_id : Int32)
+      # Find the message to update
+      if message = @callback_query.message
+        bot.edit_message_text(
+          chat_id: chat_id,
+          message_id: message.message_id,
+          text: routes_text(stop_id),
+          parse_mode: "Markdown",
+          reply_markup: update_keyboard(stop_id)
+        )
+      end
+    end
+
+    private def update_keyboard(stop_id : Int32)
+      buttons = [
+        [
+          TelegramBot::InlineKeyboardButton.new(
+            text: "🔄 #{I18n.translate("messages.update_routes")}",
+            callback_data: "update_#{stop_id}"
+          ),
+        ],
+      ]
+      TelegramBot::InlineKeyboardMarkup.new(buttons)
+    end
     private def routes_text(stop_id)
       detransport_api = DetransportTelegram::DetransportAPI.new
 
